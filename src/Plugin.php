9f2b15fbca720ce62de840148bd090c81ee7739d
Joe Huss <detain@interserver.net>
2017-07-02 06:37:41 -0400

    formatting updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -34,7 +34,7 @@ class Plugin {
 		if ($event['category'] == SERVICE_TYPES_CPANEL) {
 			myadmin_log(self::$module, 'info', 'Cpanel Activation', __LINE__, __FILE__);
 			function_requirements('activate_cpanel');
-			activate_cpanel($serviceClass->get_ip(), $event['field1']);
+			activate_cpanel($serviceClass->getIp(), $event['field1']);
 			$event->stopPropagation();
 		}
 	}
@@ -44,15 +44,15 @@ class Plugin {
 		if ($event['category'] == SERVICE_TYPES_CPANEL) {
 			myadmin_log(self::$module, 'info', 'CPanel Deactivation', __LINE__, __FILE__);
 			function_requirements('deactivate_cpanel');
-			deactivate_cpanel($serviceClass->get_ip());
+			deactivate_cpanel($serviceClass->getIp());
 			$serviceExtra = @myadmin_unstringify($serviceClass->get_extra());
 			if ($serviceExtra !== FALSE && isset($serviceExtra['ksplice']) && $serviceExtra['ksplice'] == 1 && isset($serviceExtra['ksplice_uuid']) && $serviceExtra['ksplice_uuid'] != '') {
 				function_requirements('deactivate_ksplice');
-				deactivate_ksplice((is_uuid($serviceExtra['ksplice_uuid']) ? $serviceExtra['ksplice_uuid'] : $serviceClass->get_ip()));
+				deactivate_ksplice((is_uuid($serviceExtra['ksplice_uuid']) ? $serviceExtra['ksplice_uuid'] : $serviceClass->getIp()));
 			}
 			if ($serviceExtra !== FALSE && isset($serviceExtra['kcare']) && $serviceExtra['kcare'] == 1) {
 				function_requirements('deactivate_kcare');
-				deactivate_kcare($serviceClass->get_ip());
+				deactivate_kcare($serviceClass->getIp());
 			}
 			$event->stopPropagation();
 		}
@@ -64,10 +64,10 @@ class Plugin {
 			$settings = get_module_settings(self::$module);
 			function_requirements('deactivate_cpanel');
 			function_requirements('activate_cpanel');
-			myadmin_log(self::$module, 'info', "IP Change - (OLD:".$serviceClass->get_ip().") (NEW:{$event['newip']})", __LINE__, __FILE__);
-			if (deactivate_cpanel($serviceClass->get_ip()) == TRUE) {
+			myadmin_log(self::$module, 'info', "IP Change - (OLD:".$serviceClass->getIp().") (NEW:{$event['newip']})", __LINE__, __FILE__);
+			if (deactivate_cpanel($serviceClass->getIp()) == TRUE) {
 				activate_cpanel($event['newip'], $event['field1']);
-				$GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $serviceClass->get_ip());
+				$GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $serviceClass->getIp());
 				$serviceClass->set_ip($event['newip'])->save();
 				$event['status'] = 'ok';
 				$event['status_text'] = 'The IP Address has been changed.';
