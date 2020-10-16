20dc05ddc2c10ad7ef8da36e3e64499bf66c64e1
Joe Huss <detain@interserver.net>
2020-10-16 12:22:23 -0400

    improvements to the cpanel billing update script

--- bin/update_cpanel_data.php
+++ bin/update_cpanel_data.php
@@ -133,6 +133,10 @@ WHERE license_status = 'active' AND services_id IS NOT NULL AND license_ip = '{$
 		}
 		$db->query("select * from {$settings['TABLE']} left join repeat_invoices on repeat_invoices_module='{$module}' and repeat_invoices_service={$settings['PREFIX']}_id and repeat_invoices_description like 'CPanel %' where {$settings['PREFIX']}_status='active' and {$settings['PREFIX']}_ip='{$license['ip']}'", __LINE__, __FILE__);
 		if ($db->num_rows() > 1) {
+			$db->query("select * from {$settings['TABLE']} left join repeat_invoices on repeat_invoices_module='{$module}' and repeat_invoices_deleted=0 and repeat_invoices_service={$settings['PREFIX']}_id and repeat_invoices_description like 'CPanel %' where {$settings['PREFIX']}_status='active' and {$settings['PREFIX']}_ip='{$license['ip']}'", __LINE__, __FILE__);
+		}
+/*
+		if ($db->num_rows() > 1) {
 			$found = true;
 			echo "Multiple Entries found for {$license['ip']}".PHP_EOL;
 			$ids = [];
@@ -142,34 +146,37 @@ WHERE license_status = 'active' AND services_id IS NOT NULL AND license_ip = '{$
 			}
 			$out['problems']['multiple'][$license['ip']] = [$module, $ids]; 
 		} elseif ($db->num_rows() == 1) {
+*/
+		if ($db->num_rows() >= 1) {
 			$found = true;
-			$db->next_record(MYSQL_ASSOC);
-			if (is_null($db->Record['repeat_invoices_id'])) {
-				echo "Null Repeat Invoice found for License {$license['ip']}".PHP_EOL;
-				$out['problems']['null_repeat'][$license['ip']] = [$module, $db->Record[$settings['PREFIX'].'_id']]; 
-			} else {
-				$changes = [];
-				$currency = $db->Record[$settings['PREFIX'].'_currency'];
-				$costData['cost'] = convertCurrency($costData['orig_cost'], $currency, 'USD')->getAmount()->toFloat();
-				if ($db->Record['repeat_invoices_frequency'] != 1) {
-					$changes[] = ['repeat_invoices_frequency', $db->Record['repeat_invoices_frequency'], 1];
-				}
-				if ((float)$db->Record['repeat_invoices_cost'] != (float)$costData['cost']) {
-					$changes[] = ['repeat_invoices_cost', $db->Record['repeat_invoices_cost'], (float)$costData['cost']];
-				}
-				if ($db->Record['repeat_invoices_description'] != "CPanel {$license['accounts']} Accounts for {$settings['TBLNAME']} {$db->Record[$settings['PREFIX'].'_id']}") {
-					$changes[] = ['repeat_invoices_description', $db->Record['repeat_invoices_description'], "CPanel {$license['accounts']} Accounts for {$settings['TBLNAME']} {$db->Record[$settings['PREFIX'].'_id']}"];
-				}
-				if (count($changes) > 0) {
-					$repeatObj = new \MyAdmin\Orm\Repeat_Invoice();
-					$repeatObj->load_real($db->Record['repeat_invoices_id']);
-					foreach ($changes as $change) {
-						echo "Making Changes to {$license['ip']} Repeat Invoice {$db->Record['repeat_invoices_id']} setting {$change[0]} from '{$change[1]}' to '{$change[2]}'".PHP_EOL;
-						$out['updates'][] = [$license['ip'], $module, $db->Record[$settings['PREFIX'].'_id'], $change[0], $change[1], $change[2], $db->Record['repeat_invoices_id']];
-						$func = 'set'.ucwords(str_replace('repeat_invoices_', '', $change[0]));
-						$repeatObj->$func($change[2]);
+			while ($db->next_record(MYSQL_ASSOC)) {
+				if (is_null($db->Record['repeat_invoices_id'])) {
+					echo "Null Repeat Invoice found for License {$license['ip']}".PHP_EOL;
+					$out['problems']['null_repeat'][$license['ip']] = [$module, $db->Record[$settings['PREFIX'].'_id']]; 
+				} else {
+					$changes = [];
+					$currency = $db->Record[$settings['PREFIX'].'_currency'];
+					$costData['cost'] = convertCurrency($costData['orig_cost'], $currency, 'USD')->getAmount()->toFloat();
+					if ($db->Record['repeat_invoices_frequency'] != 1) {
+						$changes[] = ['repeat_invoices_frequency', $db->Record['repeat_invoices_frequency'], 1];
 					}
-					$repeatObj->save();
+					if ((float)$db->Record['repeat_invoices_cost'] != (float)$costData['cost']) {
+						$changes[] = ['repeat_invoices_cost', $db->Record['repeat_invoices_cost'], (float)$costData['cost']];
+					}
+					if ($db->Record['repeat_invoices_description'] != "CPanel {$license['accounts']} Accounts for {$settings['TBLNAME']} {$db->Record[$settings['PREFIX'].'_id']}") {
+						$changes[] = ['repeat_invoices_description', $db->Record['repeat_invoices_description'], "CPanel {$license['accounts']} Accounts for {$settings['TBLNAME']} {$db->Record[$settings['PREFIX'].'_id']}"];
+					}
+					if (count($changes) > 0) {
+						$repeatObj = new \MyAdmin\Orm\Repeat_Invoice();
+						$repeatObj->load_real($db->Record['repeat_invoices_id']);
+						foreach ($changes as $change) {
+							echo "Making Changes to {$license['ip']} Repeat Invoice {$db->Record['repeat_invoices_id']} setting {$change[0]} from '{$change[1]}' to '{$change[2]}'".PHP_EOL;
+							$out['updates'][] = [$license['ip'], $module, $db->Record[$settings['PREFIX'].'_id'], $change[0], $change[1], $change[2], $db->Record['repeat_invoices_id']];
+							$func = 'set'.ucwords(str_replace('repeat_invoices_', '', $change[0]));
+							$repeatObj->$func($change[2]);
+						}
+						$repeatObj->save();
+					}
 				}
 			}
 		}
@@ -180,6 +187,10 @@ WHERE license_status = 'active' AND services_id IS NOT NULL AND license_ip = '{$
 			$db->query("select * from licenses left join repeat_invoices on repeat_invoices_module='licenses' and repeat_invoices_id=license_invoice and repeat_invoices_service=license_id left join services on license_type=services_id and services_type=500 where license_status='active' and services_id is not null and license_ip='{$license['ip']}'", __LINE__, __FILE__);
 		}
 		if ($db->num_rows() > 1) {
+			$db->query("select * from licenses left join repeat_invoices on repeat_invoices_module='licenses' and repeat_invoices_deleted=0 and repeat_invoices_id=license_invoice and repeat_invoices_service=license_id left join services on license_type=services_id and services_type=500 where license_status='active' and services_id is not null and license_ip='{$license['ip']}'", __LINE__, __FILE__);
+		}
+/*
+		if ($db->num_rows() > 1) {
 			echo "Multiple Entries found for {$license['ip']}".PHP_EOL;
 			$ids = [];
 			while ($db->next_record(MYSQL_ASSOC)) {
@@ -188,63 +199,66 @@ WHERE license_status = 'active' AND services_id IS NOT NULL AND license_ip = '{$
 			}
 			$out['problems']['multiple'][$license['ip']] = ['licenses', $ids]; 
 		} elseif ($db->num_rows() == 1) {
-			$db->next_record(MYSQL_ASSOC);
-			if (is_null($db->Record['repeat_invoices_id'])) {
-				echo "Null Repeat Invoice found for License {$license['ip']}".PHP_EOL;
-				$out['problems']['null_repeat'][$license['ip']] = ['licenses', $db->Record['license_id']]; 
-			} else {
-				$changes = [];
-				$currency = $db->Record['license_currency'];
-				$costData['cost'] = convertCurrency($costData['orig_cost'], $currency, 'USD')->getAmount()->toFloat();
-				if ($db->Record['repeat_invoices_frequency'] != 1) {
-					$changes[] = ['repeat_invoices_frequency', $db->Record['repeat_invoices_frequency'], 1];
-				}
-				if ((float)$db->Record['repeat_invoices_cost'] != (float)$costData['cost']) {
-					$changes[] = ['repeat_invoices_cost', $db->Record['repeat_invoices_cost'], (float)$costData['cost']];
-				}
-				if ($db->Record['repeat_invoices_description'] != "{$serviceTypes[$newService]['services_name']} {$license['accounts']} Accounts") {
-					$changes[] = ['repeat_invoices_description', $db->Record['repeat_invoices_description'], "{$serviceTypes[$newService]['services_name']} {$license['accounts']} Accounts"];
-				}
-				if (count($changes) > 0) {
-					$repeatObj = new \MyAdmin\Orm\Repeat_Invoice();
-					$repeatObj->load_real($db->Record['repeat_invoices_id']);
-					foreach ($changes as $change) {
-						echo "Making Changes to {$license['ip']} Repeat Invoice {$db->Record['repeat_invoices_id']} setting {$change[0]} from '{$change[1]}' to '{$change[2]}'".PHP_EOL;
-						$out['updates'][] = [$license['ip'], 'licenses', $db->Record['license_id'], $change[0], $change[1], $change[2], $db->Record['repeat_invoices_id']];
-						$func = 'set'.ucwords(str_replace('repeat_invoices_', '', $change[0]));
-						$repeatObj->$func($change[2]);
+*/
+		if ($db->num_rows() >= 1) {			
+			while ($db->next_record(MYSQL_ASSOC)) {
+				if (is_null($db->Record['repeat_invoices_id'])) {
+					echo "Null Repeat Invoice found for License {$license['ip']}".PHP_EOL;
+					$out['problems']['null_repeat'][$license['ip']] = ['licenses', $db->Record['license_id']]; 
+				} else {
+					$changes = [];
+					$currency = $db->Record['license_currency'];
+					$costData['cost'] = convertCurrency($costData['orig_cost'], $currency, 'USD')->getAmount()->toFloat();
+					if ($db->Record['repeat_invoices_frequency'] != 1) {
+						$changes[] = ['repeat_invoices_frequency', $db->Record['repeat_invoices_frequency'], 1];
 					}
-					$repeatObj->save();
-				}
-				$changes = [];
-				if ($db->Record['license_frequency'] != 1) {
-					$changes[] = ['license_frequency', $db->Record['license_frequency'], 1];
-				}
-				if ((float)$db->Record['license_cost'] != (float)$costData['cost']) {
-					$changes[] = ['license_cost', $db->Record['license_cost'], (float)$costData['cost']];
-				}
-				if ($db->Record['license_type'] != $newService) {
-					$changes[] = ['license_type', $db->Record['license_type'], $newService];
-				}
-				if (count($changes) > 0) {
-					$serviceObj = new \MyAdmin\Orm\License();
-					$serviceObj->load_real($db->Record['license_id']);
-					foreach ($changes as $change) {
-						echo "Making Changes to {$license['ip']} License {$db->Record['license_id']} setting {$change[0]} from '{$change[1]}' to '{$change[2]}'".PHP_EOL;
-						$out['updates'][] = [$license['ip'], 'licenses', $db->Record['license_id'], $change[0], $change[1], $change[2], $db->Record['license_id']];
-						$func = 'set'.ucwords(str_replace('license_', '', $change[0]));
-						$serviceObj->$func($change[2]);
+					if ((float)$db->Record['repeat_invoices_cost'] != (float)$costData['cost']) {
+						$changes[] = ['repeat_invoices_cost', $db->Record['repeat_invoices_cost'], (float)$costData['cost']];
 					}
-					$serviceObj->save();
+					if ($db->Record['repeat_invoices_description'] != "{$serviceTypes[$newService]['services_name']} {$license['accounts']} Accounts") {
+						$changes[] = ['repeat_invoices_description', $db->Record['repeat_invoices_description'], "{$serviceTypes[$newService]['services_name']} {$license['accounts']} Accounts"];
+					}
+					if (count($changes) > 0) {
+						$repeatObj = new \MyAdmin\Orm\Repeat_Invoice();
+						$repeatObj->load_real($db->Record['repeat_invoices_id']);
+						foreach ($changes as $change) {
+							echo "Making Changes to {$license['ip']} Repeat Invoice {$db->Record['repeat_invoices_id']} setting {$change[0]} from '{$change[1]}' to '{$change[2]}'".PHP_EOL;
+							$out['updates'][] = [$license['ip'], 'licenses', $db->Record['license_id'], $change[0], $change[1], $change[2], $db->Record['repeat_invoices_id']];
+							$func = 'set'.ucwords(str_replace('repeat_invoices_', '', $change[0]));
+							$repeatObj->$func($change[2]);
+						}
+						$repeatObj->save();
+					}
+					$changes = [];
+					if ($db->Record['license_frequency'] != 1) {
+						$changes[] = ['license_frequency', $db->Record['license_frequency'], 1];
+					}
+					if ((float)$db->Record['license_cost'] != (float)$costData['cost']) {
+						$changes[] = ['license_cost', $db->Record['license_cost'], (float)$costData['cost']];
+					}
+					if ($db->Record['license_type'] != $newService) {
+						$changes[] = ['license_type', $db->Record['license_type'], $newService];
+					}
+					if (count($changes) > 0) {
+						$serviceObj = new \MyAdmin\Orm\License();
+						$serviceObj->load_real($db->Record['license_id']);
+						foreach ($changes as $change) {
+							echo "Making Changes to {$license['ip']} License {$db->Record['license_id']} setting {$change[0]} from '{$change[1]}' to '{$change[2]}'".PHP_EOL;
+							$out['updates'][] = [$license['ip'], 'licenses', $db->Record['license_id'], $change[0], $change[1], $change[2], $db->Record['license_id']];
+							$func = 'set'.ucwords(str_replace('license_', '', $change[0]));
+							$serviceObj->$func($change[2]);
+						}
+						$serviceObj->save();
+					}
 				}
-			}
-			$query = "update licenses set license_extra='".$db->real_escape($line)."' where license_ip='{$license['ip']}' and license_type in (5000,5001,5002,5005,5008,5009,5014,10682,10683)";
-			$db2->query($query, __LINE__, __FILE__);
-			if ($license['hostname'] != '') {
-				$query = "update licenses set license_hostname='".$db->real_escape($license['hostname'])."' where license_ip='{$license['ip']}'";
+				$query = "update licenses set license_extra='".$db->real_escape($line)."' where license_ip='{$license['ip']}' and license_type in (5000,5001,5002,5005,5008,5009,5014,10682,10683)";
 				$db2->query($query, __LINE__, __FILE__);
-				//echo '('.$license['hostname'].' = '.$license['ip'].") ";
-				//echo '.';
+				if ($license['hostname'] != '') {
+					$query = "update licenses set license_hostname='".$db->real_escape($license['hostname'])."' where license_ip='{$license['ip']}'";
+					$db2->query($query, __LINE__, __FILE__);
+					//echo '('.$license['hostname'].' = '.$license['ip'].") ";
+					//echo '.';
+				}
 			}
 		} else {
 			echo "Nothing found for {$license['ip']} hostname {$license['hostname']}".PHP_EOL;
