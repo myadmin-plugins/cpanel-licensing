dc225268edca11a4685dda836c5f4459bf077c33
Joe Huss <detain@interserver.net>
2024-02-03 09:10:22 +0000

    updates

--- src/cpanel.inc.php
+++ src/cpanel.inc.php
@@ -56,8 +56,9 @@ function deactivate_cpanel($ipAddress = false)
     $request = ['ip' => $ipAddress];
     $response = $cpl->fetchLicenseId($request);
     request_log('licenses', false, __FUNCTION__, 'cpanel', 'fetchLicenseId', $request, $response);
-	if (isset($response['attr']['reason']) && $response['attr']['reason'] == 'No Licenses Found')
-		return true;
+    if (isset($response['attr']['reason']) && $response['attr']['reason'] == 'No Licenses Found') {
+        return true;
+    }
     if (isset($response['licenseid']) && isset($response['licenseid']['value'])) {
         $liscid = $response['licenseid']['value'];
         $request = ['liscid' => $liscid];
