6aba5c449dcb81d612fd634cc7c785204cebcec4
Joe Huss <detain@interserver.net>
2024-12-05 02:37:03 -0500

    update to plugins

--- src/cpanel.inc.php
+++ src/cpanel.inc.php
@@ -2,7 +2,7 @@
 /**
  * License Related Functionality
  * @author Joe Huss <detain@interserver.net>
- * @copyright 2019
+ * @copyright 2025
  * @package MyAdmin
  * @category Licenses
  */
--- src/cpanel_ksplice_addon.php
+++ src/cpanel_ksplice_addon.php
@@ -2,7 +2,7 @@
 /**
  * Licensing Functionality
  * @author Joe Huss <detain@interserver.net>
- * @copyright 2019
+ * @copyright 2025
  * @package MyAdmin
  * @category Licenses
  */
